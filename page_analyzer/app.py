ea60ea4c6c93f83b3980e0b6f00469dc337a9a7f
kamusia <adamhorolov36027@gmail.com>
2025-02-16 17:53:49 +0300

    start

---
+++ page_analyzer/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
